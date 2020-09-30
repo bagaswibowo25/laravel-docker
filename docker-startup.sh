1b763bd5685745f30c73fe28ae73e252847c0f73
bagaswibowo25 <bagas.wibowo@outlook.co.id>
2020-09-30 19:31:06 +0700

    update docker-startup

--- docker-startup.sh
+++ docker-startup.sh
@@ -5,5 +5,7 @@ mkdir -p /run/php
 chown -R nginx:nginx /run/php/php7.4-fpm.sock
 #Nginx Laravel Site Template
 envsubst <  /etc/nginx/site-template-nginx.conf > /etc/nginx/conf.d/default.conf
+mkdir -p /run/nginx
+chown -R nginx:nginx /run/nginx/
 nginx -g 'pid /run/nginx/nginx.pid;'
 tail -f /dev/null
