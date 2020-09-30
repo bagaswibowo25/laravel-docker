59ceb1f383dee695380a11e3079ec388f58e3cec
bagaswibowo25 <bagas.wibowo@outlook.co.id>
2020-09-30 14:26:05 +0700

    Add Dockerfile and etc

---
+++ laravel/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#starting php-fpm
+mkdir -p /run/php
+/usr/sbin/php-fpm7
+chown -R nginx:nginx /run/php/php7.4-fpm.sock
+#Nginx Laravel Site Template
+envsubst <  /etc/nginx/site-template-nginx.conf > /etc/nginx/conf.d/default.conf
+nginx -g 'pid /run/nginx/nginx.pid;'
+tail -f /dev/null
